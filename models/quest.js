bc0342b72a38af120d7c68119af6cc6809105383
Cobaltium <matthewferderber@gmail.com>
2015-06-25 16:24:42 -0400

    Worked on Quest Schema and user schema

--- models/quest.js
+++ models/quest.js
@@ -1,42 +1,44 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var questSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    points: {
-        type: Number,
-        required: true
-    },
-    objective: {
-        numGames: Number,
-        kills: Number,
-        champion: Number,
-        assists: Number,
-        deaths: Number,
-        goldEarned: Number,
-        minions: Number,
-        winner: Boolean,
-        pentaKills: Number,
-        quadraKills: Number,
-        tripleKills: Number,
-        wardsPlaced: Number,
-        wardsKilled: Number,
-        healingDone: Number,
-        largestKillingSpree: Number,
-        firstBloodKill: Number,
-        neutralMinionsKilled: Number,
-        neutralMinionsKilledEnemyJungle: Number
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    required: true
+  },
+  objectives: [{
+      objective: String,
+      value: Number
+    }]
+    // numGames: Number,
+    // kills: Number,
+    // champion: Number,
+    // assists: Number,
+    // deaths: Number,
+    // goldEarned: Number,
+    // minions: Number,
+    // winner: Boolean,
+    // pentaKills: Number,
+    // quadraKills: Number,
+    // tripleKills: Number,
+    // wardsPlaced: Number,
+    // wardsKilled: Number,
+    // healingDone: Number,
+    // largestKillingSpree: Number,
+    // firstBloodKill: Number,
+    // neutralMinionsKilled: Number,
+    // neutralMinionsKilledEnemyJungle: Number
 });
 
 module.exports = mongoose.model('Quest', questSchema);
--- models/user.js
+++ models/user.js
@@ -1,76 +1,87 @@
 'use strict';
 var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
+  bcrypt = require('bcrypt'),
+  SALT_WORK_FACTOR = 10;
 var Schema = mongoose.Schema;
+var userQuestSchema = new Schema({
+  quest: {
+    type: Schema.Types.ObjectId,
+    ref: 'Quest'
+  },
+  progress: [{
+    objective: String,
+    value: Number
+  }]
+});
 var userSchema = new Schema({
-    summonerName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    region: {
-        type: String,
-        required: true
-    },
-    summonerId: {
-        type: String,
-        required: true
-    },
-    isVerified: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    verificationString: {
-        type: String
-    },
-    champions : [Number],
+  summonerName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  summonerId: {
+    type: String,
+    required: true
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  verificationString: {
+    type: String
+  },
+  champions: [Number],
+  quests: [userQuestSchema]
 });
 userSchema.pre('save', function(cb) {
-    var user = this;
+  var user = this;
 
-    //only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) {
-        return cb();
+  //only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) {
+    return cb();
+  }
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return cb(err);
     }
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) {
-            return cb(err);
-        }
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) {
-                return cb(err);
-            }
-            // override the cleartext password with the hashed one
-            user.password = hash;
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return cb(err);
+      }
+      // override the cleartext password with the hashed one
+      user.password = hash;
 
-            cb();
-        });
+      cb();
     });
+  });
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        console.log(isMatch);
-        cb(null, isMatch);
-    });
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    console.log(isMatch);
+    cb(null, isMatch);
+  });
 };
 userSchema.set('toJSON', {
-    transform: function(doc, ret, options) {
-        delete ret.password;
-        return ret;
-    }
+  transform: function(doc, ret, options) {
+    delete ret.password;
+    return ret;
+  }
 });
 module.exports = mongoose.model('User', userSchema);
